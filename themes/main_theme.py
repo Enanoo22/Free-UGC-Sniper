5d648689bf85b81f7d2c63590dd892ab866e331a
CenvoldYT <133511788+CenvoldYT@users.noreply.github.com>
2023-05-15 18:12:58 -0400

    Changed some colors

--- themes/main_theme.py
+++ themes/main_theme.py
@@ -11,25 +11,29 @@ import os
 
 
 title = ("""
- ██████╗███████╗███╗   ██╗██╗   ██╗ ██████╗ ██╗     ██████╗     ███████╗███╗   ██╗██╗██████╗ ███████╗██████╗ 
-██╔════╝██╔════╝████╗  ██║██║   ██║██╔═══██╗██║     ██╔══██╗    ██╔════╝████╗  ██║██║██╔══██╗██╔════╝██╔══██╗
-██║     █████╗  ██╔██╗ ██║██║   ██║██║   ██║██║     ██║  ██║    ███████╗██╔██╗ ██║██║██████╔╝█████╗  ██████╔╝
-██║     ██╔══╝  ██║╚██╗██║╚██╗ ██╔╝██║   ██║██║     ██║  ██║    ╚════██║██║╚██╗██║██║██╔═══╝ ██╔══╝  ██╔══██╗
-╚██████╗███████╗██║ ╚████║ ╚████╔╝ ╚██████╔╝███████╗██████╔╝    ███████║██║ ╚████║██║██║     ███████╗██║  ██║
- ╚═════╝╚══════╝╚═╝  ╚═══╝  ╚═══╝   ╚═════╝ ╚══════╝╚═════╝     ╚══════╝╚═╝  ╚═══╝╚═╝╚═╝     ╚══════╝╚═╝  ╚═╝
-                                                                                                             
+ ██████╗███████╗███╗   ██╗██╗   ██╗ ██████╗ ██╗     ██████╗ ███████╗    ███████╗███╗   ██╗██╗██████╗ ███████╗██████╗ 
+██╔════╝██╔════╝████╗  ██║██║   ██║██╔═══██╗██║     ██╔══██╗██╔════╝    ██╔════╝████╗  ██║██║██╔══██╗██╔════╝██╔══██╗
+██║     █████╗  ██╔██╗ ██║██║   ██║██║   ██║██║     ██║  ██║███████╗    ███████╗██╔██╗ ██║██║██████╔╝█████╗  ██████╔╝
+██║     ██╔══╝  ██║╚██╗██║╚██╗ ██╔╝██║   ██║██║     ██║  ██║╚════██║    ╚════██║██║╚██╗██║██║██╔═══╝ ██╔══╝  ██╔══██╗
+╚██████╗███████╗██║ ╚████║ ╚████╔╝ ╚██████╔╝███████╗██████╔╝███████║    ███████║██║ ╚████║██║██║     ███████╗██║  ██║
+ ╚═════╝╚══════╝╚═╝  ╚═══╝  ╚═══╝   ╚═════╝ ╚══════╝╚═════╝ ╚══════╝    ╚══════╝╚═╝  ╚═══╝╚═╝╚═╝     ╚══════╝╚═╝  ╚═╝
+                                                                                                                     
 """)
 def _print_stats(self) -> None:
     output_lines = []
     output_lines.append(f"Version: {self.version}")
     output_lines.append(Fore.CYAN + Style.BRIGHT + title)
     output_lines.append(Fore.RESET + Style.RESET_ALL)
-    output_lines.append(Style.BRIGHT + f"                           [ Limiteds: {Fore.GREEN}{Style.BRIGHT}{len(self.items)}{Fore.WHITE}{Style.BRIGHT} ]")
+    output_lines.append(Style.BRIGHT + f"                           [ Limiteds: {Fore.CYAN}{Style.BRIGHT}{len(self.items)}{Fore.WHITE}{Style.BRIGHT} ]")
     output_lines.append(Style.BRIGHT + f"                           [ UGCs Bought: {Fore.GREEN}{Style.BRIGHT}{self.buys}{Fore.WHITE}{Style.BRIGHT} ]")
-    output_lines.append(Style.BRIGHT + f"                           [ Total Errors: {Fore.RED}{Style.BRIGHT}{self.errors}{Fore.WHITE}{Style.BRIGHT} ]")
-    output_lines.append(Style.BRIGHT + f"                           [ Last Speed: {Fore.CYAN}{Style.BRIGHT}{self.last_time}{Fore.WHITE}{Style.BRIGHT} ]")
-    output_lines.append(Style.BRIGHT + f"                           [ Price Checks: {Fore.CYAN}{Style.BRIGHT}{self.checks}{Fore.WHITE}{Style.BRIGHT} ]")
+    output_lines.append(Style.BRIGHT + f"                           [ Errors: {Fore.RED}{Style.BRIGHT}{self.errors}{Fore.WHITE}{Style.BRIGHT} ]")
+    output_lines.append(Style.BRIGHT + f"                           [ Speed: {Fore.CYAN}{Style.BRIGHT}{self.last_time}{Fore.WHITE}{Style.BRIGHT} ]")
+    output_lines.append(Style.BRIGHT + f"                           [ Checks: {Fore.CYAN}{Style.BRIGHT}{self.checks}{Fore.WHITE}{Style.BRIGHT} ]")
     output_lines.append("")
+    if self.rooms:
+        output_lines.append(Style.BRIGHT + f"                           [ Room Users: {Fore.GREEN}{Style.BRIGHT}{len(self.users)}{Fore.WHITE}{Style.BRIGHT} ]")
+        output_lines.append(Style.BRIGHT + f"                           [ Room Code: {Fore.GREEN}{Style.BRIGHT}{self.room_code}{Fore.WHITE}{Style.BRIGHT} ]")
+        output_lines.append("")
     output_lines.append(Style.BRIGHT + f"                           [ Current Task: {Fore.CYAN}{Style.BRIGHT}{self.task}{Fore.WHITE}{Style.BRIGHT} ]")
 
     output = '\n'.join(output_lines)
